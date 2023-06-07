9c07ae92d2f70e50050652764a35fddb65250299
ggnsall <ggnsall@naver.com>
2023-06-08 00:04:55 +0900

    [FE] ADD : ScrapScreen 페이지 스켈레톤 코드 추가

---
+++ newsletter/src/routers/ScrapScreen.tsx
@@ -0,0 +1,18 @@
+function ScrapScreen() {
+  return <>
+    <h1>처음</h1>
+    <h1>Scrap</h1>
+    <h1>Scrap</h1>
+    <h1>Scrap</h1>
+    <h1>Scrap</h1>
+    <h1>Scrap</h1>
+    <h1>Scrap</h1>
+    <h1>Scrap</h1>
+    <h1>Scrap</h1>
+    <h1>Scrap</h1>
+    <h1>Scrap</h1>
+    <h1>마지막</h1>
+  </>;
+}
+
+export default ScrapScreen;
