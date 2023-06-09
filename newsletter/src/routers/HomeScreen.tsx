40774f3a16202d9be7cdb7c243e371be98d64a02
ggnsall <ggnsall@naver.com>
2023-06-09 14:25:58 +0900

    [FE] UPDATE : HomeScreen 컴포넌트 구조 작성

--- newsletter/src/routers/HomeScreen.tsx
+++ newsletter/src/routers/HomeScreen.tsx
@@ -1,5 +1,23 @@
+import { Suspense } from "react";
+
+import Loading from "../components/Loading/Loading";
+import ErrorMessage from "../components/ErrorBoundary/ErrorMessage";
+import ErrorBoundary from "../components/ErrorBoundary/ErrorBoundary";
+import HomeScreenFetcer from "../components/HomeScreen/HomeScreenFetcher";
+import HomeScreenListContainer from "../components/HomeScreen/HomeScreenListContainer";
+
 function HomeScreen() {
-  return <div></div>;
+  return (
+    <>
+      <ErrorBoundary fallback={ErrorMessage}>
+        <Suspense fallback={<Loading />}>
+          <HomeScreenFetcer>
+            <HomeScreenListContainer />
+          </HomeScreenFetcer>
+        </Suspense>
+      </ErrorBoundary>
+    </>
+  );
 }
 
 export default HomeScreen;
