f9fba09af97930c8dfe081091668f24d24d1e8dd
ggnsall <ggnsall@naver.com>
2023-06-10 21:18:51 +0900

    [FE] UPDATE : newscard CSS 변경 - 헤드라인 제외 글자 position 및 별 아이콘 색상 변경

--- newsletter/src/components/NewsCard/NewsCard.tsx
+++ newsletter/src/components/NewsCard/NewsCard.tsx
@@ -1,7 +1,13 @@
 /** @jsxImportSource @emotion/react */
+import { useState } from "react";
 import { css } from "@emotion/react";
-import { AiOutlineStar } from "@react-icons/all-files/ai/AiOutlineStar";
 import { AiFillStar } from "@react-icons/all-files/ai/AiFillStar";
+import { AiOutlineStar } from "@react-icons/all-files/ai/AiOutlineStar";
+
+import { Props } from "./NewsCardType";
+import { L, go, takeAll } from "../../customs/FunctionalJS";
+import { Docs } from "../HomeScreen/HomeScreenType";
+import { getDayOfWeek } from "../../customs/getDayOfWeek";
 
 const newsCardStyle = css`
   width: 88%;
@@ -15,22 +21,32 @@ const newsCardStyle = css`
 
   .star-icon {
     position: absolute;
-    top: 13%;
+    top: 11%;
     right: 5%;
     color: #6d6d6d;
   }
 
   .news-headline {
     margin: 0;
-    padding: 3% 15% 2% 5%;
+    margin-bottom: 2%;
+    padding: 2% 15% 1% 5%;
     font-weight: 600;
     font-size: 1.125rem;
+    height: 55%;
+
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
   }
 
   .news-info {
     display: flex;
     justify-content: space-between;
     margin: 0 5%;
+
     p {
       margin: 0;
       font-size: 0.8125rem;
@@ -40,8 +56,10 @@ const newsCardStyle = css`
       font-weight: 400;
       width: auto;
       white-space: nowrap;
+      letter-spacing: -0.03rem;
+
       p:nth-of-type(1) {
-        margin-right: 10%;
+        margin-right: 2%;
       }
     }
 
@@ -51,24 +69,88 @@ const newsCardStyle = css`
   }
 `;
 
-function NewsCard() {
+function NewsCard({ data, divRef, index }: Props) {
+  const [isScrap, setIsScrap] = useState<boolean>(false);
+
+  // 기사 클릭 시 URL 이동 함수
+  const goToUrl = () => window.open(`${data.web_url}`);
+
+  // 스크랩 온 / 오프 함수
+  const getStorageItem = (key: string) => {
+    return JSON.parse(localStorage.getItem(key) as string);
+  };
+
+  const onScrap = (data: Docs, e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+
+    let updateLocalData;
+
+    if (isScrap) {
+      const storedObject = getStorageItem("scrap-data");
+      updateLocalData = go(
+        storedObject,
+        L.filter((d: Docs) => d._id !== data._id),
+        takeAll
+      );
+    } else {
+      if (localStorage.getItem("scrap-data")) {
+        const storedObject = getStorageItem("scrap-data");
+        updateLocalData = [data, ...storedObject];
+      } else {
+        updateLocalData = [data];
+      }
+    }
+    localStorage.removeItem("scrap-data");
+    localStorage.setItem("scrap-data", JSON.stringify(updateLocalData));
+    setIsScrap((isScrap) => !isScrap);
+  };
+
   return (
-    <div css={newsCardStyle}>
-      <div className="star-icon">
-        <AiOutlineStar size={25} />
+    <div
+      css={newsCardStyle}
+      ref={(ref) => {
+        if (ref) {
+          divRef.current[index] = ref;
+        }
+      }}
+      onClick={goToUrl}
+    >
+      <div
+        className="star-icon"
+        onClick={(e: React.MouseEvent<HTMLDivElement>) => onScrap(data, e)}
+      >
+        {isScrap ? (
+          <AiFillStar size={25} css={{ color: "#FFB800" }} />
+        ) : (
+          <AiOutlineStar size={25} />
+        )}
       </div>
 
-      <p className="news-headline">
-        국방부 “北, 화성-17 실패 만회하려 영상 짜깁기… 성공 조작”
-      </p>
+      {/* 헤드라인 */}
+      <p className="news-headline">{data?.headline.main}</p>
+
       <div className="news-info">
         <div className="reporter-info">
-          <p>조선일보</p>
-          <p>김정확 기자</p>
+          {/* 뉴스 데스크 */}
+          <p>{data?.news_desk ? data?.news_desk : null}</p>
+
+          {/* 리포터 */}
+          <p>
+            {data?.byline.person.length
+              ? `By ${data?.byline.person[0].firstname} ${
+                  data?.byline.person[0].lastname
+                } ${data?.byline.person.length > 1 ? "et al." : ""}`
+              : null}
+          </p>
         </div>
 
         <div className="date-info">
-          <p>2021.3.15 (목)</p>
+          {/* 날짜 */}
+          <p>{`${data?.pub_date
+            .split("T")[0]
+            .replaceAll("-", ". ")} (${getDayOfWeek(
+            data?.pub_date.split("T")[0]
+          )})`}</p>
         </div>
       </div>
     </div>
